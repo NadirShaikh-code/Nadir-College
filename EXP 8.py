d540bd78b5a0b383490a6892230df14606206096
NadirShaikh-code <shaikhnadirnasir@gmail.com>
2026-01-19 19:08:41 +0530

    Add files via upload

---
+++ EXP 8.py
@@ -0,0 +1,11 @@
+#Nadir Shaikh
+#UIN - 251P086
+print("Nadir Shaikh")
+print("UIN - 251P086")
+
+rows = 5
+
+for i in range(1,rows + 1):
+    for j in range(i):
+     print("*", end="")
+    print()
