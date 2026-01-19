b1511f96f7f3609b2a69dd481660e059811a44dc
NadirShaikh-code <shaikhnadirnasir@gmail.com>
2026-01-19 19:32:08 +0530

    Add files via upload

---
+++ EXP 10.py
@@ -0,0 +1,19 @@
+#Nadir Shaikh
+#UIN - 251P086
+print("Nadir Shaikh")
+print("UIN - 251P086")
+
+def is_prime(n): 
+ if n <= 1: 
+  return False 
+ for i in range(2, n): 
+  if n % i == 0: 
+   return False 
+  return True 
+number = int(input("Enter a number to check if it's prime: ")) 
+if is_prime(number): 
+ print(f"{number} is a prime number.") 
+else: 
+ print(f"{number} is not a prime number.")
+
+
